8e0133572c22431c479f9beaccd9b3e46c85e1ca
VladyslavLysenko <Dodog12362@gmail.com>
2022-10-15 12:04:02 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -8,27 +8,28 @@ const list = document.querySelector(".country-list")
 const countryInfo = document.querySelector(".country-info")
 
 inputCountry.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY, {
-  'leading': true,
-  'trailing': false,
+  'leading': false,
+  'trailing': true,
 }));
 
 function fullMarkUp(arr) {
+    console.log(arr);
     return arr.map(item => 
-        `<li>
-        <img src="${item.flags.svg}" alt="flag's ${item.name}" width="50px">
-        <h2 class="Country">${item.name}</h2>
-        <span class="Capital">Capital: ${item.capital}</span>
-        <span class="Population">Population: ${item.population}</span>
-        <span class="Languages">Languages: ${(item.languages[0].nativeName)}</span>
-        </li>`
+        ` <div class="main-title"><img src="${item.flags.svg}" alt="flag's ${item.name}" width="35px" height="30px"><h2 class="country">${item.name}</h2></div>
+        <ul class="country-list-full">
+        <li class="country-item-full"><h3 class="capital h-font">Capital:</h3> <p class="country-item-data">${item.capital}</p></li>
+        <li class="country-item-full"><h3 class="population h-font">Population:</h3> <p class="country-item-data">${item.population}</p></li>
+        <li class="country-item-full"><h3 class="Languages h-font">Languages:</h3> <p class="country-item-data">${(item.languages[0].name)}</p></li>
+        </ul>`
+       
     ).join("")
 }
 
 function shortMarkUp(arr) {
     return arr.map(item => 
-        `<li>
-        <img src="${item.flags.svg}" alt="flag's ${item.name}" width="50px">
-        <h2 class="Country">${item.name}</h2>
+        `<li class="item-country">
+        <img src="${item.flags.svg}" alt="flag's ${item.name}" width="35px" height="25px">
+        <p class="country-font">${item.name}</p>
         </li>`
     ).join("")
 }
@@ -42,16 +43,16 @@ function fetchCountries(e) {
     let name = inputCountry.value.toLowerCase().trim();
     // console.log(inputCountry.value);
     if (!name) {
+        list.innerHTML = "";
         countryInfo.innerHTML = "";
         // console.log("Its work!");
     }
     else {
-        
-        // .trim() ????
         const resp = fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`)
         resp.then(response =>{
             if (!response.ok) {
-                countryInfo.innerHTML = "";  
+                list.innerHTML = "";  
+                countryInfo.innerHTML = ""; 
                 Notiflix.Notify.failure("Oops, there is no country with that name")
             }
             // console.log(response);
@@ -66,10 +67,12 @@ function fetchCountries(e) {
             if  (data.length < 2) {
                 
                 countryInfo.innerHTML = fullMarkUp(data);
+                list.innerHTML = "";
                 // console.log(data);
             } else if (data.length < 10) {
                 
-                countryInfo.innerHTML = shortMarkUp(data);
+                list.innerHTML = shortMarkUp(data);
+                countryInfo.innerHTML = "";
                 //  console.log(data);
             } else if (data.length > 10) {
                 Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
@@ -82,4 +85,3 @@ function fetchCountries(e) {
 }
 
 
-// Выполни санитизацию введенной строки методом trim(), это решит проблему когда в поле ввода только пробелы или они есть в начале и в конце строки.
